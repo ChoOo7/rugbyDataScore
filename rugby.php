3680b2092cc8fc6a4ecdd40c047e98394a310408
ChoOo7 <github@simon-minotto.com>
2015-09-01 02:50:33 +0200

    adding main script file

---
+++ rugby.php
@@ -0,0 +1,122 @@
+<?php
+$match = $xfsign = null;
+
+if(php_sapi_name() == "cli")
+{
+  $match = isset($argv[1]) ? $argv[1] : "d2OVreOh";
+  $xfsign = "SW9D1eZo";
+}else{
+  $match = $_GET['match'];//"d2OVreOh";
+  $xfsign = $_GET['xfsign'];//"SW9D1eZo";
+}
+
+
+//Prevent the full hostname to be searchable on github search :)
+$hostnamePart1 = "flash";
+$hostnamePart2 = "resultats";
+$hostname = $hostnamePart1.$hostnamePart2;
+
+$url = "http://d.".$hostname.".fr/x/feed/d_su_".$match."_fr_1";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_REFERER, 'http://d.flashresultats.fr/x/feed/proxy');
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Requested-With: XMLHttpRequest', 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36', 'X-Fsign: '.$xfsign, 'Accept-Language: *', 'Connection: keep-alive', 'X-GeoIP: 1')); 
+    
+$ret = curl_exec($ch);
+$tmp = explode('</table>', $ret);
+$ret = $tmp[0].'</table>';
+$ret = str_replace('&nbsp;', ' ', $ret);
+$xml = simplexml_load_string($ret);
+
+//var_dump($xml);
+
+$results = array();
+$results['actions'] = array();
+
+$subSequence = 0;
+
+foreach($xml->tbody->tr as $tr)
+{
+  $class = (string) $tr['class'];
+  if($class == 'odd' || $class == 'even')
+  {
+    $action = null;
+    $time = null;
+    $who = null;
+    $team = 0;
+    
+    foreach($tr->td as $td)
+    {
+      $team++;
+      foreach($td->div as $div)
+      {
+        
+	$divContent = trim((string)$div);
+	if( $divContent)
+	{
+	  $action = $divContent;
+	  foreach($div->div as $subDiv)
+	  {
+	    if($subDiv['class'] == 'time-box')
+	    {
+	      $time = (string)$subDiv;
+	    }
+	  }
+	  foreach($div->span as $subDiv)
+	  {
+	    if($subDiv['class'] == 'participant-name')
+	    {
+	      $who = (string)$subDiv;
+	      break 3;
+	    }
+	  }
+	  
+	}
+      }
+    }
+  }else{
+    $subSequence++;
+    continue;
+  }
+  
+  if($action == null)
+  {
+    continue;
+  }
+  
+  $team = min(2, $team);
+  
+  $results['actions'][] = array(
+    'action'=>$action,
+    'time'=>$time,
+    'who'=>$who,
+    'team'=>$team,
+    'playpart'=>$subSequence
+  );
+}
+
+
+$url = "http://www.".$hostname.".fr/match/".$match."/";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_REFERER, 'http://d.flashresultats.fr/x/feed/proxy');
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Requested-With: XMLHttpRequest', 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36', 'X-Fsign: '.$xfsign, 'Accept-Language: *', 'Connection: keep-alive', 'X-GeoIP: 1')); 
+
+$ret = curl_exec($ch);
+
+$ret = str_replace(array("\n", "\t","\r"), array('', '', ''), $ret);
+preg_match('!.*<td class="current\-result"><span class="scoreboard">([0-9]+)</span><span class="scoreboard\-divider">\-</span><span class="scoreboard">([0-9]+)</span></td>.*!uis', $ret, $matches);
+
+$results['score1'] = $matches[1];
+$results['score2'] = $matches[2];
+
+preg_match('!.*<td colspan="3" class="mstat">([^<]+)</td>.*!uis', $ret, $matches);
+$results['state'] = html_entity_decode($matches[1]);
+
+
+header('Content-Type: application/json');
+echo json_encode($results, JSON_PRETTY_PRINT);
